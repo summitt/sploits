21dde41ab8912cbadf1cb3fb42c00d17f59c02a9
ascetik <ascetik@gmail.com>
2016-05-30 21:48:58 -0500

    added sploits

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -1,14 +1,33 @@
 package burp;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
 
+import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import com.josh.ActionJackson;
 
 public class BurpExtender implements IBurpExtender,IContextMenuFactory{
 	private IBurpExtenderCallbacks cb;
+	private HashMap<String,String> sploits = new HashMap<String,String>();
+	private HashMap<String,String> remotes = new HashMap<String,String>();
 
 	@Override
 	public List<JMenuItem> createMenuItems(IContextMenuInvocation inv) {
@@ -17,18 +36,365 @@ public class BurpExtender implements IBurpExtender,IContextMenuFactory{
 		inc.addActionListener(new ActionJackson(inv, cb, false)); // This will add the domain to the scope.
 		JMenuItem exc = new JMenuItem("!Domain2Scope");
 		exc.addActionListener(new ActionJackson(inv, cb, true)); // This will add the domain to the scope.
+		JMenu sub = new JMenu("sploits");
+		JMenu config = new JMenu("sploits config");
+		//Convert to treemap to automatically sort results before displaying in the menu
+		Map<String, String> sorted = new TreeMap<String, String>(sploits); 
+		for(String sploit : sorted.keySet()){
+			JMenuItem spm = new JMenuItem(sploit);
+			spm.addActionListener(new ActionJackson(inv, cb, sploits));
+			sub.add(spm);
+		}
+		JMenuItem update = new JMenuItem("Refresh sploits");
+		update.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getSploits();	
+			}
+			
+		});
+		config.add(update);
+		JMenuItem add = new JMenuItem("Add To sploits");
+		add.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					add2sploits(inv);
+			}
+			
+		});
+		config.add(add);
+		JMenuItem proxy = new JMenuItem("Add Proxy");
+		proxy.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					addPoxy();
+			}
+			
+		});
+		config.add(proxy);
+		JMenuItem remote = new JMenuItem("Add Remote sploits");
+		remote.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					addRemote();
+			}
+			
+		});
+		config.add(remote);
+		JMenuItem dremote = new JMenuItem("Delete Remote sploits");
+		dremote.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					delRemote();
+			}
+			
+		});
+		config.add(dremote);
+		JMenuItem delete = new JMenuItem("Delete sploit");
+		delete.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					deleteSploit();
+			}
+			
+		});
+		config.add(delete);
+		JMenuItem convert = new JMenuItem("Convert Remote to Local");
+		convert.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				convertRemote2local();
+			}
+			
+		});
+		config.add(convert);
+		
 		List<JMenuItem>stuff = new ArrayList<JMenuItem>();
 		stuff.add(inc);
 		stuff.add(exc);
+		stuff.add(sub);
+		stuff.add(config);
 		return stuff;
 	}
 
 	@Override
 	public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 		this.cb = callbacks;
-		cb.setExtensionName("Domain2Scope");
+		cb.setExtensionName("sploits");
 		cb.registerContextMenuFactory(this);
+		propsTest();
+		getSploits();
 		
+		
+	}
+	private boolean isRequest(IContextMenuInvocation inv){
+		if(inv.getInvocationContext() == inv.CONTEXT_MESSAGE_EDITOR_REQUEST || inv.getInvocationContext() == inv.CONTEXT_MESSAGE_VIEWER_REQUEST)
+			return true;
+		else 
+			return false;
+		
+	}
+	
+	private void deleteSploit(){
+		List<String> list =  new ArrayList<String>();
+		for(String key : sploits.keySet()){
+			if(!key.startsWith("r_"))
+				list.add(""+key + "=" + sploits.get(key) );
+		}
+		
+		String dsploit = (String)JOptionPane.showInputDialog(
+                new JFrame(),
+                "Delete a local sploit: ",
+                "Delete sploits",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                list.toArray(),
+                null);
+		if(dsploit!=null && !dsploit.equals("")){
+			delProps(dsploit.split("=")[0]);
+			getSploits();
+		}
+	}
+	
+	private void delRemote(){
+		List<String> list =  new ArrayList<String>();
+		for(String key : remotes.keySet()){
+			list.add(""+key + "=" + remotes.get(key) );
+		}
+		
+		String remote = (String)JOptionPane.showInputDialog(
+                new JFrame(),
+                "Delete a remote host: ",
+                "Update sploits",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                list.toArray(),
+                null);
+		if(remote!=null && !remote.equals("")){
+			String rkey = remote.split("=")[0];
+			delProps(rkey);
+			getSploits();
+		}
+		
+		
+	}
+	
+	private void addPoxy(){
+		String proxy = (String)JOptionPane.showInputDialog(
+                new JFrame(),
+                "Enter proxy (hostname:port): ",
+                "Update sploits",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                null);
+		add2props("proxy", proxy);
+		getSploits();
+	}
+	
+	private void addRemote(){
+		String remote = (String)JOptionPane.showInputDialog(
+                new JFrame(),
+                "Enter remote URL: ",
+                "Update sploits",
+                JOptionPane.PLAIN_MESSAGE,
+                null,
+                null,
+                null);
+		add2props("remote" + remotes.size(), remote);
+		getSploits();
+	}
+	
+	private void add2sploits(IContextMenuInvocation inv){
+		
+		int start = inv.getSelectionBounds()[0];
+		int stop = inv.getSelectionBounds()[1];
+		for(IHttpRequestResponse o : inv.getSelectedMessages()){
+			String all = (new String(isRequest(inv)? o.getRequest(): o.getResponse()));
+			String Selected = all.substring(start, stop);
+			String Key = (String)JOptionPane.showInputDialog(
+                    new JFrame(),
+                    "Enter sploit Name: ",
+                    "Update sploits",
+                    JOptionPane.PLAIN_MESSAGE,
+                    null,
+                    null,
+                    null);
+			add2props(Key, Selected);
+			getSploits();
+		}
+		
+	}
+	
+	private void getURLSploits(String URL, String proxy){
+		
+		Properties prop = new Properties();
+		try {
+			if(proxy != null && !proxy.equals("")){
+				String [] splits = proxy.split(":");
+				System.setProperty("http.proxyHost", splits[0]);
+				System.setProperty("http.proxyPort", splits[1]);
+			}
+
+			URL url = new URL(URL);
+			
+			InputStream in = url.openStream();
+			Reader reader = new InputStreamReader(in, "UTF-8"); // for example
+			// load a properties file
+			prop.load(reader);
+			String title = prop.getProperty("title","");
+			if(!title.equals(""))
+				title+="_";
+			for(Object key : prop.keySet()){
+				if(!key.equals("title"))
+					sploits.put("r_"+title +key, prop.getProperty(""+key));
+				
+			}
+			if(proxy != null && !proxy.equals("")){
+				System.setProperty("http.proxyHost", "");
+			}
+			
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			
+		}
+		
+	}
+	private void add2props(String key, String value){
+		Properties prop = new Properties();
+		InputStream input = null;
+		FileOutputStream fos = null;
+		try {
+			
+			input = new FileInputStream("sploits.properties");
+			
+			// load a properties file
+			prop.load(input);
+			prop.setProperty(key, value);
+			fos = new FileOutputStream("sploits.properties");
+			prop.store(fos, "####Updated with sploits ");
+			
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+					fos.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	private void delProps(String key){
+		Properties prop = new Properties();
+		InputStream input = null;
+		FileOutputStream fos = null;
+		try {
+			
+			input = new FileInputStream("sploits.properties");
+			
+			// load a properties file
+			prop.load(input);
+			prop.remove(key);
+			fos = new FileOutputStream("sploits.properties");
+			prop.store(fos, "####Updated with sploits ");
+			
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+					fos.close();
+					
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	private void getSploits(){
+		sploits = new HashMap<String,String>();
+		remotes = new HashMap<String,String>();
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			
+			input = new FileInputStream("sploits.properties");
+			// load a properties file
+			prop.load(input);
+			for(Object key : prop.keySet()){
+				if(((String) key).startsWith("remote")){
+					getURLSploits(prop.getProperty(""+key),prop.getProperty("proxy",""));
+					remotes.put(""+key, prop.getProperty(""+key));
+				}else if (!key.equals("proxy")){
+					sploits.put(""+key, prop.getProperty(""+key));
+				}
+				
+			}
+			
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	private void propsTest(){
+		File prop = new File("sploits.properties");
+		if(!prop.exists()){
+			try {
+				prop.createNewFile();
+				add2props("remote0", "https://raw.githubusercontent.com/summitt/sploits-default/master/sploits.properties");
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	private void convertRemote2local(){
+		int isYes = (int)JOptionPane.showOptionDialog(
+                new JFrame(),
+                "Do You Want to Convert All Remote sploits to Local sploits? \n"
+                + "This will also delete all remote urls.",
+                "Update sploits",
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,null,null);
+		if(isYes == 0){
+			for(String key : sploits.keySet()){
+				if(key.startsWith("r_")){
+					String newKey = key.replaceFirst("r_", "");
+					add2props(newKey, sploits.get(key));
+				}
+			}
+			for(String key : remotes.keySet()){
+				delProps(key);
+			}
+			getSploits();
+		}
 	}
 
 }
--- src/com/josh/ActionJackson.java
+++ src/com/josh/ActionJackson.java
@@ -2,8 +2,17 @@ package com.josh;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
 
 import burp.IBurpExtenderCallbacks;
 import burp.IContextMenuInvocation;
@@ -13,6 +22,7 @@ public class ActionJackson implements ActionListener{
 	private IContextMenuInvocation inv;
 	private IBurpExtenderCallbacks cb;
 	private boolean isExclude=false;
+	private HashMap<String,String> sploits;
 	
 	public ActionJackson(IContextMenuInvocation inv, IBurpExtenderCallbacks callbacks, boolean isExclude ){
 		this.inv = inv;
@@ -20,37 +30,123 @@ public class ActionJackson implements ActionListener{
 		this.isExclude=isExclude;
 		
 	}
+	public ActionJackson(IContextMenuInvocation inv, IBurpExtenderCallbacks callbacks, HashMap<String,String>sploits){
+		this.inv = inv;
+		this.cb = callbacks;
+		this.sploits = sploits;
+		
+	}
+	public ActionJackson(IContextMenuInvocation inv, IBurpExtenderCallbacks callbacks){
+		this.inv = inv;
+		this.cb = callbacks;
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		for(IHttpRequestResponse o : inv.getSelectedMessages()){
-			String host = o.getUrl().getHost();
-			//if(!cb.isInScope(o.getUrl())){
-				URL https;
-				URL http;
-				try {
-					https = new URL("https://"+host);
-					http = new URL("http://"+host);
-					
-					if(!isExclude){
-						cb.includeInScope(http);
-						cb.includeInScope(https);
-						cb.printOutput("Including " + host + " in Scope");
-					}else{
-						cb.excludeFromScope(http);
-						cb.excludeFromScope(https);
-						cb.printOutput("Excluding " + host + " in Scope");
+		String caller = e.getActionCommand();
+		
+		//If adding a domian to scope then do this:
+		if(caller.equals("Domains2Scope") || caller.equals("!Domains2Scope")){
+			for(IHttpRequestResponse o : inv.getSelectedMessages()){
+				String host = o.getUrl().getHost();
+				//if(!cb.isInScope(o.getUrl())){
+					URL https;
+					URL http;
+					try {
+						https = new URL("https://"+host);
+						http = new URL("http://"+host);
+						
+						if(!isExclude){
+							cb.includeInScope(http);
+							cb.includeInScope(https);
+							cb.printOutput("Including " + host + " in Scope");
+						}else{
+							cb.excludeFromScope(http);
+							cb.excludeFromScope(https);
+							cb.printOutput("Excluding " + host + " in Scope");
+						}
+					} catch (MalformedURLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
-				} catch (MalformedURLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				
-			//}
+					
+				//}
+			}
+		}else{ // Else we want to add our attacks.
+			replace(sploits.get(caller));
+			
 		}
 		
 		
+	}
+	
+	
+	
+	
+
+	private void replace(Object replace){
+		if(replace == null)
+			return;
+		int start = inv.getSelectionBounds()[0];
+		int stop = inv.getSelectionBounds()[1];
+		if(replace.getClass().getName().equals("java.lang.String")){
+			for(IHttpRequestResponse o : inv.getSelectedMessages()){
+				String all = getMessage(o);
+				String Selected = all.substring(start, stop);
+				String begin = all.substring(0, start);
+				String end = all.substring(stop);
+				all = begin + replace + end;
+				setMessage(o, all);
+				break;
+			}
+		}else{
+			for(IHttpRequestResponse o : inv.getSelectedMessages()){
+				byte[] all = getMsgBytes(o);
+				byte[] begin = Arrays.copyOfRange(all, 0, start);
+				byte[] end = Arrays.copyOfRange(all, stop, all.length);
+				byte [] r = (byte[])replace;
+				byte [] out = new byte[begin.length + end.length + r.length];
+				System.arraycopy(begin, 0, out, 0, begin.length);
+				System.arraycopy(r, 0, out, begin.length, r.length);
+				System.arraycopy(end, 0, out, r.length+begin.length, end.length);
+				setMsgBytes(o,out);
+				break;
+			}
+			
+		}
+	
+	}
+	private boolean isRequest(){
+		if(inv.getInvocationContext() == inv.CONTEXT_MESSAGE_EDITOR_REQUEST || inv.getInvocationContext() == inv.CONTEXT_MESSAGE_VIEWER_REQUEST)
+			return true;
+		else 
+			return false;
+		
+	}
+	
+	private String getMessage(IHttpRequestResponse o){
+		return (new String(isRequest()? o.getRequest(): o.getResponse()));
+	}
+	private byte[] getMsgBytes(IHttpRequestResponse o){
+		return isRequest()? o.getRequest(): o.getResponse();
+	}
+	private void setMessage(IHttpRequestResponse o, String update){
+		if(isRequest()){
+			o.setRequest(update.getBytes());
+			
+		}else{
+			o.setResponse(update.getBytes());
+		}
+	}
+	private void setMsgBytes(IHttpRequestResponse o, byte [] update){
+		if(isRequest()){
+			o.setRequest(update);
+			
+		}else{
+			o.setResponse(update);
+		}
 	}
 	
 
